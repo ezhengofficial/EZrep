10eedc7e546a8f21316d7bc9e13e43149b9d0884
Edwin Zheng <91067699+ezhengofficial@users.noreply.github.com>
2021-10-08 11:20:21 -0400

    fixed app.py and tablified.html

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -8,82 +8,28 @@ import random
 import csv
 app = Flask(__name__) #create instance of class Flask
 
-def openCSV(fname):
-    # creates an empty dictionary and sets the temp_total and total to 0
-    dict = {}
-    temp_total = 0
-    total = 0
-    with open(fname) as f:
-        # ensures that the column headers are not included in the dictionary 
-        f.readline()
-        reader = csv.reader(f, delimiter=',')
-        for row in reader:
-            # separately saves the total of the probabilities for each occupation
-            if 'Total' in row:
-                total = float(row[1])
-            else:
-                # populates dictionary 'dict' with (key: value) pairs that represent
-                #  occupations and their probabilities
-                dict[row[0]] = float(row[1])
-                # keeps track of consecutive sum of the values
-                temp_total += float(row[1])
-        # if a total is not given, sets it to the sum of the value
-        if total == 0:
-            total = temp_total
-        return dict, total
+import csv #imports built in csv library
+import random #imports the random library
+occupations = csv.DictReader(open("data/occupations.csv", "r")) #imports csv as a dict via rows
+job_percentages = dict() #stores jobs mapping to their percentages of workforce
+#formatting it to a more iterable format
+for row in occupations:
+	row_values = tuple(row.values()) #gets rid of the unecessary keys information
+	job_percentages[row_values[0]] = float(row_values[1]) #maps jobs to their percentages of workforce in dict
 
-# picks an occupation based on the weighted percentages
-def picker():
-    jobDict, total = openCSV('occupations.csv')
-    # we multiply the total by 10 for random.randint() to produce an int to 
-    #  simulate randomly selecting an occupation
-    total = total * 10
-    # stores a list of occupations and their corresponding probabilities in 
-    #  two separate lists
-    nums = list(jobDict.values())
-    occ = list(jobDict.keys())
-    conDict = {}
-    sum = 0
-    # populates conDict such that each (key: value) pair represents the occupation
-    #  and 10 times the sum of the probabilities preceding that occupation
-    for i in range(len(nums)):
-        nums[i] *= 10.0
-        sum += nums[i]
-        nums[i] = sum
-    for i in range(len(occ)):
-        conDict[occ[i]] = nums[i]
+# debug_print(job_percentages)
 
-    # picks a random int from 0 inclusive to the total exclusive so that the number of
-    #  integers picked equals the possibility for probabilities
-    n = random.randint(0, total-1)
-    # returns the occupation if its probability is within range of the consecutive sum
-    for i in conDict:
-        if (n < conDict[i]):
-            return i
+#generates a random job given a probability dict formatted {job: percentage with total at the bottom}
+def random_job(probability_book = job_percentages): #default argument provided. Valid for an assignment use case.
+	# removes total from dictionary
+	try: #deletes total line if it exists
+		del probability_book["Total"]
+	except KeyError:
+		pass
+	jobs = list(probability_book.keys())
+	percentages = list(probability_book.values())
 
-# runs n trials to determine if the probability of the resulting occupations matches 
-#  those listed in the .csv file
-def test_probs(n):
-    testResults, total = openCSV('occupations.csv')
-    for i in testResults:
-        testResults[i] = 0
-    for i in range(n):
-        testResults[picker()] += 1
-    for i in testResults:
-        testResults[i] = [testResults[i], round(testResults[i] / n * 10000) / 100]
-    print(testResults)
-
-#returns an HTML list of the occupations
-def getList():
-    testResults, total = openCSV('occupations.csv')
-    HTMLList = '''
-    <ul>
-    '''
-    occupations = testResults.keys()
-    for i in occupations:
-        HTMLList += f"<li>{i}</li>\n"
-    HTMLList += "</ul>"
-    return HTMLList
+	return random.choices(population=jobs, k=1, weights=percentages)[0]
 
 
 @app.route("/")       #assign fxn to route
@@ -102,7 +48,6 @@ def display():      #code to display the HTML on the webpage
     return output
 @app.route("/occupyflaskst")
 def test_tmplt():
-        return render_template('tablified.html', Occupations="picker()", collection = getList())
-
+        return render_template('tablified.html', randOcc=random_job(), collection = job_percentages.keys())
 app.debug = True
 app.run()
