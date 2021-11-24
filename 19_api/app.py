607ac6dc05c6e07ac99257ed2ec07ae5955e34b3
Edwin Zheng <91067699+ezhengofficial@users.noreply.github.com>
2021-11-23 22:23:58 -0500

    19_api

--- 19_api/app.py
+++ 19_api/app.py
@@ -4,18 +4,18 @@
 # 2021-10-04
 
 # Same file as from yesterday. See notes from 09_flask-v0/bigbrain.txt
-import urllib.request
+import urllib.request, json
 from flask import Flask, render_template
 app = Flask(__name__) 
 
 @app.route("/") 
 def apodapi():
-    key = open("key_nasa.txt")
-    with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key='+ key) as response:
-
-    render_template("main.html")
-
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
+    url = "https://api.nasa.gov/planetary/apod?api_key=" + str(open("key_nasa.txt", "r").read())
+    request = urllib.request.urlopen(url)
+    data = json.loads(request.read())
+    return render_template("main.html", img=data['url'])
+    
+if __name__ == "__main__":
     app.debug = True
     app.run()
+
